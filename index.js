c7c506f84b2dac84a04956861efd1303ef465e55
NushratNowrin <nowrin2023@gmail.com>
2023-03-19 23:07:21 +0600

    Show previous result

--- index.js
+++ index.js
@@ -76,7 +76,6 @@ const displayQuiz = (data) => {
 // EventListener for quiz submit button
 document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
-
     return;
   }
   quizTimer(true);
@@ -109,7 +108,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   }
 
   // data setting on local storage and getting data from local storage
-  let storage = JSON.parse(localStorage.getItem("result"));
+  let storage = JSON.parse(localStorage.getItem("results"));
   if (storage) {
     localStorage.setItem(
       "results",
